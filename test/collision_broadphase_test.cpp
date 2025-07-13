0e9d054d1b3fad006ffedad3fccf093e2110860f
Ian Su <thisisianemail@gmail.com>
2025-07-13 13:21:18 -0400

    .

---
+++ test/collision_broadphase_test.cpp
@@ -0,0 +1,122 @@
+#include <spdlog/spdlog.h>
+
+#include <Eigen/Core>
+#include <catch2/catch_test_macros.hpp>
+#include <filesystem>
+#include <functional>
+#include <vector>
+
+#include "abc_file_loader.hpp"
+#include "sap.hpp"
+#include "sap_kd_tree.hpp"
+
+using namespace silk;
+namespace fs = std::filesystem;
+
+const fs::path root{PHYSICS_SCENE_ROOT};
+const fs::path cloth_sphere_abc = root / "cloth_sphere_collision.abc";
+
+struct SimpleColliderdata {
+  int face_id;
+  int v0;
+  int v1;
+  int v2;
+};
+
+TEST_CASE("sap-test", "[collision broadphase]") {
+  auto objects = loadAllMeshes(cloth_sphere_abc);
+
+  // object 0 is the cloth
+  auto& cloth = objects[0];
+  int vnum = cloth.V.rows();
+  int fnum = cloth.F.rows();
+
+  std::vector<BboxCollider<SimpleColliderdata>> colliders(fnum);
+  for (int i = 0; i < fnum; ++i) {
+    SimpleColliderdata data;
+    data.face_id = i;
+    data.v0 = cloth.F(i, 0);
+    data.v1 = cloth.F(i, 1);
+    data.v2 = cloth.F(i, 2);
+
+    Eigen::Matrix3f m;
+    m.row(0) = cloth.V.row(data.v0);
+    m.row(1) = cloth.V.row(data.v1);
+    m.row(2) = cloth.V.row(data.v2);
+
+    Bbox bbox;
+    bbox.min = m.colwise().minCoeff();
+    bbox.max = m.colwise().maxCoeff();
+
+    colliders[i] = {bbox, data};
+  }
+
+  std::vector<BboxColliderProxy<SimpleColliderdata>> proxies(fnum);
+  for (int i = 0; i < fnum; ++i) {
+    proxies[i] = colliders.data() + i;
+  }
+
+  int axis = sap_optimal_axis(proxies.data(), fnum);
+  sap_sort_proxies(proxies.data(), fnum, axis);
+
+  CollisionCache<SimpleColliderdata> cache;
+  CollisionFilterCallback<SimpleColliderdata> dummy_filter =
+      [](const SimpleColliderdata& a, const SimpleColliderdata& b) -> bool {
+    return true;
+  };
+  sap_sorted_group_self_collision(proxies.data(), fnum, axis, dummy_filter,
+                                  cache);
+  spdlog::info("detect {} collisions", cache.size());
+  for (auto& c : cache) {
+    auto& fa = c.first;
+    auto& fb = c.second;
+    spdlog::info("pair {} {}", fa.face_id, fb.face_id);
+  }
+}
+
+// TEST_CASE("sap-kdtree-tests", "[collision broadphase]") {
+//   auto objects = loadAllMeshes(cloth_sphere_abc);
+//
+//   // object 0 is the cloth
+//   auto& cloth = objects[0];
+//   int vnum = cloth.V.rows();
+//   int fnum = cloth.F.rows();
+//
+//   using Collider = BboxCollider<SimpleColliderdata>;
+//   std::vector<Collider> colliders;
+//   colliders.resize(fnum);
+//   for (int i = 0; i < fnum; ++i) {
+//     SimpleColliderdata data;
+//     data.face_id = i;
+//     data.v0 = cloth.F(i, 0);
+//     data.v1 = cloth.F(i, 1);
+//     data.v2 = cloth.F(i, 2);
+//
+//     Eigen::Matrix3f m;
+//     m.row(0) = cloth.V.row(data.v0);
+//     m.row(1) = cloth.V.row(data.v1);
+//     m.row(2) = cloth.V.row(data.v2);
+//
+//     Bbox bbox;
+//     bbox.min = m.colwise().minCoeff();
+//     bbox.max = m.colwise().maxCoeff();
+//
+//     colliders[i] = {bbox, data};
+//   }
+//
+//   KDTree<SimpleColliderdata> tree{colliders.data(), fnum};
+//   tree.update();
+//
+//   CollisionCache<SimpleColliderdata> cache;
+//   auto dummy_filter = [](const SimpleColliderdata& a,
+//                          const SimpleColliderdata& b) -> bool { return true;
+//                          };
+//
+//   tree.test_self_collision(dummy_filter, cache);
+//   spdlog::info("detect {} collisions", cache.size());
+//   for (auto& c : cache) {
+//     auto& fa = c.first;
+//     auto& fb = c.second;
+//     spdlog::info("pair {} {}", fa.face_id, fb.face_id);
+//   }
+// }
