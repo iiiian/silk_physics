df7c8f1be19508cbd586dd857c79971f256fab2a
Ian Su <TzuHsiangSu3000@gmail.com>
2025-10-17 21:03:43 -0400

    simplify gpu solver widget
    - remove unused API like getter/setter and on change callback
    - remove unused includes

--- demo/src/widgets/gpu_solver_widget.cpp
+++ demo/src/widgets/gpu_solver_widget.cpp
@@ -7,38 +7,26 @@
 
 #include "ui_console.hpp"
 
-GpuSolverWidget::GpuSolverWidget(Context& ctx,
-                                 std::function<void(SolverBackend)> onChange)
-    : ctx_(ctx), n_change_(std::move(onChange)) {}
-
-void GpuSolverWidget::set_backend(SolverBackend b) { backend_ = b; }
+GpuSolverWidget::GpuSolverWidget(Context& ctx) : ctx_(ctx) {}
 
 void GpuSolverWidget::draw() {
   ImGui::PushID(this);
 
-  if (ImGui::CollapsingHeader((title_ + "##gpu_solver").c_str(),
+  if (ImGui::CollapsingHeader("Solver##gpu_solver",
                               ImGuiTreeNodeFlags_DefaultOpen)) {
-    bool useGpu = (backend_ == SolverBackend::GPU);
+    bool use_gpu = (backend_ == SolverBackend::GPU);
 
-    if (ImGui::Checkbox("Use GPU solver##gpu_solver_checkbox", &useGpu)) {
+    if (ImGui::Checkbox("Use GPU solver##gpu_solver_checkbox", &use_gpu)) {
       // Status Log
-      if (useGpu) {
+      if (use_gpu) {
         backend_ = SolverBackend::GPU;
-        UI_LOGI("[UI] Solver backend -> GPU");
-
+        ui_info("[UI] Solver backend -> GPU");
       } else {
         backend_ = SolverBackend::CPU;
-        UI_LOGI("[UI] Solver backend -> CPU");
+        ui_info("[UI] Solver backend -> CPU");
       }
     }
   }
-
-  // Pass an animated negative value, e.g. -1.0f * (float)ImGui::GetTime() is
-  // the recommended value. Adjust the factor if you want to adjust the
-  // animation speed. ImGui::ProgressBar(-1.0f * (float)ImGui::GetTime(),
-  // ImVec2(0.0f, 0.0f), "Simulating..");
-  // ImGui::SameLine(0.0f, ImGui::GetStyle().ItemInnerSpacing.x);
-  // ImGui::Text("");
 
   ImGui::PopID();
 }
--- demo/src/widgets/gpu_solver_widget.hpp
+++ demo/src/widgets/gpu_solver_widget.hpp
@@ -1,11 +1,6 @@
 #pragma once
-#include <polyscope/point_cloud.h>
-
-#include <Eigen/Core>
-#include <glm/glm.hpp>
 
 #include "../gui_utils.hpp"
-#include "ui_console.hpp"
 
 //**************************************************************/
 //**             Toggle for GPU Solver                         */
@@ -16,21 +11,10 @@ enum class SolverBackend { Auto, CPU, GPU };
 class GpuSolverWidget : public IWidget {
  private:
   Context& ctx_;
-  SolverBackend backend_ = SolverBackend::CPU;  // Default: CPU
-  // std::function<void(SolverBackend,SolverBackend)> on_change_;
-  std::function<void(SolverBackend)> n_change_;
-  std::string title_ = "Solver";  // Title
+  SolverBackend backend_ = SolverBackend::CPU;
 
  public:
-  // onChange
-  explicit GpuSolverWidget(
-      Context& ctx, std::function<void(SolverBackend)> nChange = nullptr);
-  // draw
+  explicit GpuSolverWidget(Context& ctx);
+
   void draw() override;
-
-  // extern setter
-  void set_backend(SolverBackend b);
-  SolverBackend backend() const { return backend_; }
-
-  void set_title(std::string t) { title_ = std::move(t); }
 };
