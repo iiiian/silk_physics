07441f41d2bf14187fa7c02cb298137fcfe4ecde
Ian Su <TzuHsiangSu3000@gmail.com>
2026-05-16 14:19:15 -0400

    add deps

--- silk/src/backend/cuda/ecs.hpp
+++ silk/src/backend/cuda/ecs.hpp
@@ -50,8 +50,64 @@ using ObjRegistry = ::silk::ecs::Registry<ClothConfig,
 
 }  // namespace silk::cuda
 
+// ---------------------------------------
+// Component Dependencies
+// ---------------------------------------
+
 namespace silk {
 
-// dependency here
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, ClothConfig> {
+  using Type =
+      ecs::ComponentList<ClothAssemblyL2Cache, cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, CollisionConfigPlus> {
+  using Type = ecs::ComponentList<cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, TriMesh> {
+  using Type =
+      ecs::ComponentList<cuda::MeshPartition, InitialState, cuda::PinIndex,
+                         ClothAssemblyL2Cache, cuda::PhysicalState,
+                         cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, InitialState> {
+  using Type =
+      ecs::ComponentList<cuda::PhysicalState, cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, cuda::MeshPartition> {
+  using Type = ecs::ComponentList<ClothAssemblyL2Cache, cuda::PhysicalState,
+                                  cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, cuda::PinIndex> {
+  using Type =
+      ecs::ComponentList<cuda::PinPosition, cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, ClothAssemblyL2Cache> {
+  using Type = ecs::ComponentList<cuda::assembly::ClothAssemblyL1Cache,
+                                  cuda::collision::ObjectCollider>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry,
+                                cuda::assembly::ClothAssemblyL1Cache> {
+  using Type = ecs::ComponentList<cuda::solver::ClothADMMHelper>;
+};
+
+template <>
+struct ecs::ComponentDependents<cuda::ObjRegistry, cuda::PhysicalState> {
+  using Type = ecs::ComponentList<cuda::collision::ObjectCollider>;
+};
 
 }  // namespace silk
