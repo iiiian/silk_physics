c5e6a3e10d80fe9e16387f702599ac1656fe7efb
Ian Su <TzuHsiangSu3000@gmail.com>
2026-08-07 09:31:54 -0400

    remove misc

--- tt.bash
+++
@@ -1,2 +0,0 @@
-# ./build/linux_release_cuda/test/narrowphase/narrowphase_benchmark --repetitions 1 --out hybrid_ticcd.csv --ccd silk_hybrid_ticcd
-./build/linux_release_cuda/test/narrowphase/narrowphase_benchmark --repetitions 1 --out silk_ticcd.csv --ccd silk_ticcd
